e93c735fee7297c6b95f7773a50ed59a33fa072c
Mike Daley <michaelcdaley@yahoo.com>
2021-11-10 13:52:06 -0800

    Updated blog-summary component to redirect to the blog post if the user clicks anywhere on the blog-summary div instead of having to click specifically on the blog title

--- components/blog-summary.tsx
+++ components/blog-summary.tsx
@@ -12,30 +12,30 @@ import { IBlogMetadata }  from '../interfaces/blog.interfaces'
  */
 export default function BlogSummary({metadata} : {metadata: IBlogMetadata}) {
   return (
-    <li key={metadata.id}>
-      <div className="flex flex-row p4 mb-5 bg-purple-200">
-        <div>
-          <Image
-            src     = {`/${getCoverImage(metadata)}`}
-            alt     = "Blog post cover"
-            width   = {128}
-            height  = {128}
-          />
-        </div>
-        <div className="p-4 pt-2 w-full flex flex-col bg-gray-200">
-          <p className="text-gray-600">
-            <Date dateString={metadata.date} />
-          </p>
-          <h2 className="text-2xl mb-2">
-            <Link href={`/posts/${metadata.id}`}>
-              <a className="group-hover:underline hover:text-blue-700">{metadata.title}</a>
-            </Link>
-          </h2>
-          <p className="text-gray-800 text-base">
-            {metadata.excerpt}
-          </p>
-        </div>
-      </div>
+    <li key={metadata.id} className="group">
+      <Link href={`/posts/${metadata.id}`}>
+        <a>
+          <div className="flex flex-row p4 mb-5 bg-purple-200">
+            <div>
+              <Image
+                src     = {`/${getCoverImage(metadata)}`}
+                alt     = "Blog post cover"
+                width   = {128}
+                height  = {128}
+              />
+            </div>
+            <div className="p-4 pt-2 w-full flex flex-col bg-gray-200 group-hover:bg-yellow-300">
+              <p className="text-gray-600">
+                <Date dateString={metadata.date} />
+              </p>
+              <h2 className="text-2xl mb-2 group-hover:underline">{metadata.title}</h2>
+              <p className="text-gray-800 text-base">
+                {metadata.excerpt}
+              </p>
+            </div>
+          </div>
+        </a>
+      </Link>
     </li>
   )
 }
