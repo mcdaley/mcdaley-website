5c8f800189d6f7347203739ed80eecf72ea589a6
Mike Daley <michaelcdaley@yahoo.com>
2021-11-15 06:40:46 -0800

    Added responsive navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,33 +1,77 @@
 //-----------------------------------------------------------------------------
 // components/navbar.tsx
 //-----------------------------------------------------------------------------
-import Link   from 'next/link'
+import Link           from 'next/link'
+import { useState }   from 'react'
 
 /**
  * @function Navbar
  */
 export default function Navbar() {
+  const [active, setActive] = useState(false)
+
+  /**
+   * Toggle the hamburger navbar menu
+   */
+  const handleClick = () => {
+    setActive(!active)
+  }
+
+  /**
+   * Return the Navbar component
+   */
   return (
-    <nav className="flex bg-gray-900 text-white w-screen">
-      <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-        <Link href="#">
-          {/* <img class="h-9" src="logo.png" alt="logo"> */}
-          <a className="text-3xl font-bold font-heading">Logo Here</a>
+    <>
+      <nav className="flex items-center flex-wrap bg-green-500 p-3">
+        <Link href="/">
+          <a className="inline-flex items-center p-2 mr-4">
+            <svg
+              viewBox   = '0 0 24 24'
+              xmlns     = 'http://www.w3.org/2000/svg'
+              className = 'fill-current text-white h-8 w-8 mr-2'
+            >
+              <path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
+            </svg>
+            <span className='text-xl text-white font-bold uppercase tracking-wide'>
+              Logo Here
+            </span>
+          </a>
         </Link>
-        <ul className="hidden sm:flex px-4 mx-auto font-semibold font-heading space-x-12">
-          <li>
-            <Link href={"/"}>
-              <a className="hover:text-gray-200">Home</a>
+        <button 
+          className = "inline-flex p-3 hover:bg-green-800 rounded md:hidden text-white ml-auto hover:text-white outline-none"
+          onClick   = {handleClick}
+        >
+          <svg
+            className = 'w-6 h-6'
+            fill      = 'none'
+            stroke    = 'currentColor'
+            viewBox   = '0 0 24 24'
+            xmlns     = 'http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap   = 'round'
+              strokeLinejoin  = 'round'
+              strokeWidth     = {2}
+              d               = 'M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
+      
+        <div className={`${active ? '' : 'hidden'}   w-full md:inline-flex md:flex-grow md:w-auto`}>
+          <div className="md:inline-flex md:flex-row md:ml-auto md:w-auto w-full md:items-center items-start  flex flex-col md:h-auto">
+            <Link href='/'>
+              <a className='md:inline-flex md:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-800 hover:text-white '>
+                Home
+              </a>
             </Link>
-          </li>
-          <li>
-            <Link href={"/about"}>
-              <a className="hover:text-gray-200">About</a>
+            <Link href='/about'>
+              <a className='md:inline-flex md:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-800 hover:text-white '>
+                About
+              </a>
             </Link>
-          </li>
-        </ul>
-        {/* TODO: Header Icons */}
-      </div>
-    </nav>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
